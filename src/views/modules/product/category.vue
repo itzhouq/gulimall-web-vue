b94a6bd5ddfb72f33d2b1907997ebf947dfcefdc
itzhouq <itzhouq@163.com>
2020-07-27 00:06:24 +0800

    project init

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,44 @@
+<!--  -->
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(data => {
+          console.log("成功获取到菜单数据", data)
+      });
+    }
+  },
+  created() {
+      this.getMenus();
+  },
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+};
+</script>
+<style scoped>
+</style>
