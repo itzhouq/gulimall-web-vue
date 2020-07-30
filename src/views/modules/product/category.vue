21005acb6c38eeecbd4d0461765549ee27516feb
itzhouq <itzhouq@163.com>
2020-07-30 21:57:53 +0800

    商品服务-API-三级分类-删除-页面效果

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,30 @@
 <!--  -->
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    :expand-on-click-node="false"
+    show-checkbox
+    node-key="catId"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(node, data)"
+        >Append</el-button>
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >Delete</el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -21,15 +45,21 @@ export default {
     getMenus() {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
-        method: "get"
-      }).then(({data}) => {
-          console.log("成功获取到菜单数据", data.data);
-          this.menus = data.data;
+        method: "get",
+      }).then(({ data }) => {
+        console.log("成功获取到菜单数据", data.data);
+        this.menus = data.data;
       });
-    }
+    },
+    append(node, data) {
+      console.log("append", node, data);
+    },
+    remove(node, data) {
+      console.log("remove", node, data);
+    },
   },
   created() {
-      this.getMenus();
+    this.getMenus();
   },
   mounted() {},
   beforeCreate() {},
