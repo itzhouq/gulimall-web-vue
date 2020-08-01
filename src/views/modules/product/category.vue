a4f8f5b56a0e6c91625a0c24f3805edd6c0079b1
itzhouq <itzhouq@163.com>
2020-08-01 13:41:18 +0800

    商品服务-API-三级分类-新增-新增菜单

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,31 +1,46 @@
 <!--  -->
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    :expand-on-click-node="false"
-    show-checkbox
-    node-key="catId"
-    :default-expanded-keys="expandedKey"
-  >
-    <span class="custom-tree-node" slot-scope="{ node, data }">
-      <span>{{ node.label }}</span>
-      <span>
-        <el-button
-          v-if="node.level <= 2"
-          type="text"
-          size="mini"
-          @click="() => append(node, data)"
-        >Append</el-button>
-        <el-button
-          v-if="node.childNodes.length == 0"
-          type="text"
-          size="mini"
-          @click="() => remove(node, data)"
-        >Delete</el-button>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(node, data)"
+          >Append</el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >Delete</el-button>
+        </span>
       </span>
-    </span>
-  </el-tree>
+    </el-tree>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -34,6 +49,8 @@ export default {
   data() {
     return {
       menus: [],
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 1 },
+      dialogVisible: false,
       expandedKey: [],
       defaultProps: {
         children: "children",
@@ -53,8 +70,31 @@ export default {
         this.menus = data.data;
       });
     },
+    // 添加的三级分类
+    addCategory() {
+      console.log("添加的三级分类数据", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单保存成功",
+          type: "success",
+        });
+        // 关闭对话框
+      this.dialogVisible = false;
+      // 刷新出新的菜单
+      this.getMenus();
+      // 设置需要默认展开的菜单
+      this.expandedKey = [this.category.parentCid];
+      });
+    },
     append(node, data) {
       console.log("append", node, data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
     },
     remove(node, data) {
       var ids = [data.catId];
@@ -76,7 +116,7 @@ export default {
             // 刷新出新的菜单
             this.getMenus();
             // 设置需要默认展开的菜单
-            this.expandedKey = [node.parent.data.catId]
+            this.expandedKey = [node.parent.data.catId];
           });
         })
         .catch(() => {});
