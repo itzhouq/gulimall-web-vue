0af7b8cd999dc26b367335d1920c9299784cd242
itzhouq <itzhouq@163.com>
2020-08-08 09:18:34 +0800

    商品服务-API-品牌管理-OSS上传组件前后端联调

--- src/components/upload/singleUpload.vue
+++ src/components/upload/singleUpload.vue
@@ -80,10 +80,11 @@
         let _self = this;
         return new Promise((resolve, reject) => {
           policy().then(response => {
+            console.log("响应的数据" , response)
             _self.dataObj.policy = response.data.policy;
             _self.dataObj.signature = response.data.signature;
             _self.dataObj.ossaccessKeyId = response.data.accessid;
-            _self.dataObj.key = response.data.dir + '/'+getUUID()+'_${filename}';
+            _self.dataObj.key = response.data.dir +getUUID()+'_${filename}';
             _self.dataObj.dir = response.data.dir;
             _self.dataObj.host = response.data.host;
             resolve(true)
