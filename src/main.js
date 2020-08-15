89ea35f7a6f3d5f0d18c5fe10f776493b096eea6
itzhouq <itzhouq@163.com>
2020-08-15 17:41:58 +0800

    解决member/memberlevel/list请求失败问题

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
+import PubSub from 'pubsub-js'
+Vue.prototype.PubSub = PubSub
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
